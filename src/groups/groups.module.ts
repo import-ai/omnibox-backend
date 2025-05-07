54bbf497f8462ebf757bad7206629160865428de
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-05-07 16:11:59 +0800

    refactor: merge group-member and group module

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Group } from './groups.entity';
+import { Group } from './entities/group.entity';
+import { GroupMember } from './entities/group-member.entity';
 
 @Module({
   providers: [GroupsService],
   exports: [GroupsService],
   controllers: [],
-  imports: [TypeOrmModule.forFeature([Group])],
+  imports: [
+    TypeOrmModule.forFeature([Group]),
+    TypeOrmModule.forFeature([GroupMember]),
+  ],
 })
 export class GroupsModule {}
--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Group } from './groups.entity';
 import { Repository } from 'typeorm';
+import { Group } from './entities/group.entity';
+import { GroupMember } from './entities/group-member.entity';
 
 @Injectable()
 export class GroupsService {
   constructor(
     @InjectRepository(Group)
     private readonly groupRepository: Repository<Group>,
+    @InjectRepository(GroupMember)
+    private readonly groupMemberRepository: Repository<GroupMember>,
   ) {}
 }

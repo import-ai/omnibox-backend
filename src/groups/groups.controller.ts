0f073386a6abdd222c5574d3bcd9ee75569762a0
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-05-07 17:27:07 +0800

    feat(groups): implement group API

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Delete,
   Get,
@@ -6,8 +7,13 @@ import {
   Patch,
   Post,
   Req,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { GroupsService } from './groups.service';
+import { CreateGroupDto } from './dto/create-group.dto';
+import { plainToInstance } from 'class-transformer';
+import { GroupDto } from './dto/group.dto';
+import { UpdateGroupDto } from './dto/update-group.dto';
 
 @Controller('api/v1/namespaces/:namespaceId/groups')
 export class GroupController {
@@ -15,12 +21,25 @@ export class GroupController {
 
   @Get()
   async list(@Req() req, @Param('namespaceId') namespaceId: string) {
-    // todo
+    const groups = await this.groupsService.listGroupByUser(
+      namespaceId,
+      req.user.id,
+    );
+    return plainToInstance(GroupDto, groups);
   }
 
   @Post()
-  async create(@Req() req, @Param('namespaceId') namespaceId: string) {
-    // todo
+  async create(
+    @Req() req,
+    @Param('namespaceId') namespaceId: string,
+    @Body() createGroupDto: CreateGroupDto,
+  ) {
+    const group = await this.groupsService.createGroup(
+      namespaceId,
+      req.user.id,
+      createGroupDto,
+    );
+    return plainToInstance(GroupDto, group);
   }
 
   @Patch(':groupId')
@@ -28,8 +47,19 @@ export class GroupController {
     @Req() req,
     @Param('namespaceId') namespaceId: string,
     @Param('groupId') groupId: string,
+    @Body() updateGroupDto: UpdateGroupDto,
   ) {
-    // todo
+    if (
+      !(await this.groupsService.userInGroup(namespaceId, groupId, req.user.id))
+    ) {
+      throw new UnauthorizedException('user is not a member of this group');
+    }
+    const group = await this.groupsService.updateGroup(
+      namespaceId,
+      groupId,
+      updateGroupDto,
+    );
+    return plainToInstance(GroupDto, group);
   }
 
   @Delete(':groupId')
@@ -38,6 +68,11 @@ export class GroupController {
     @Param('namespaceId') namespaceId: string,
     @Param('groupId') groupId: string,
   ) {
-    // todo
+    if (
+      !(await this.groupsService.userInGroup(namespaceId, groupId, req.user.id))
+    ) {
+      throw new UnauthorizedException('user is not a member of this group');
+    }
+    await this.groupsService.deleteGroup(namespaceId, groupId);
   }
 }

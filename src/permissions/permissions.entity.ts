db4102562c3a4fd639e1f58b718a053d0d1c0e70
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-05-07 15:25:23 +0800

    feat: add permissions

---
+++ src/permissions/permissions.entity.ts
@@ -0,0 +1,46 @@
+import { Base } from 'src/common/base.entity';
+import { Group } from 'src/groups/groups.entity';
+import { Namespace } from 'src/namespaces/namespaces.entity';
+import { Resource } from 'src/resources/resources.entity';
+import { User } from 'src/user/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  Column,
+} from 'typeorm';
+
+@Entity('permissions')
+export class Permission extends Base {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('uuid', { name: 'namespace_id' })
+  namespaceId: string;
+
+  @Column('uuid', { name: 'resource_id' })
+  resourceId: string;
+
+  @Column('uuid', { name: 'group_id' })
+  groupId: string;
+
+  @Column('uuid', { name: 'user_id' })
+  userId: string;
+
+  @ManyToOne(() => Namespace)
+  @JoinColumn({ name: 'namespace_id' })
+  namespace: Namespace;
+
+  @ManyToOne(() => Resource)
+  @JoinColumn({ name: 'resource_id' })
+  resource: Resource;
+
+  @ManyToOne(() => Group)
+  @JoinColumn({ name: 'group_id' })
+  group: Group;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+}

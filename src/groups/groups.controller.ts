b874bd7f14c24e98476656ce445fd9d081cbfba2
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-05-07 18:33:51 +0800

    feat(groups): implement members API

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -14,6 +14,8 @@ import { CreateGroupDto } from './dto/create-group.dto';
 import { plainToInstance } from 'class-transformer';
 import { GroupDto } from './dto/group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
+import { MemberDto } from './dto/member.dto';
+import { AddMemberDto } from './dto/add-member.dto';
 
 @Controller('api/v1/namespaces/:namespaceId/groups')
 export class GroupController {
@@ -74,5 +76,54 @@ export class GroupController {
       throw new UnauthorizedException('user is not a member of this group');
     }
     await this.groupsService.deleteGroup(namespaceId, groupId);
+  }
+
+  @Get(':groupId/members')
+  async listMembers(
+    @Req() req,
+    @Param('namespaceId') namespaceId: string,
+    @Param('groupId') groupId: string,
+  ) {
+    if (
+      !(await this.groupsService.userInGroup(namespaceId, groupId, req.user.id))
+    ) {
+      throw new UnauthorizedException('user is not a member of this group');
+    }
+    const members = await this.groupsService.listMembers(namespaceId, groupId);
+    return plainToInstance(MemberDto, members);
+  }
+
+  @Post(':groupId/members')
+  async addMember(
+    @Req() req,
+    @Param('namespaceId') namespaceId: string,
+    @Param('groupId') groupId: string,
+    @Body() addMemberDto: AddMemberDto,
+  ) {
+    if (
+      !(await this.groupsService.userInGroup(namespaceId, groupId, req.user.id))
+    ) {
+      throw new UnauthorizedException('user is not a member of this group');
+    }
+    await this.groupsService.addMember(
+      namespaceId,
+      groupId,
+      addMemberDto.userId,
+    );
+  }
+
+  @Delete(':groupId/members/:userId')
+  async deleteMember(
+    @Req() req,
+    @Param('namespaceId') namespaceId: string,
+    @Param('groupId') groupId: string,
+    @Param('userId') userId: string,
+  ) {
+    if (
+      !(await this.groupsService.userInGroup(namespaceId, groupId, req.user.id))
+    ) {
+      throw new UnauthorizedException('user is not a member of this group');
+    }
+    await this.groupsService.deleteMember(namespaceId, groupId, userId);
   }
 }

f3a341e4353de98d9b0cf5b88748415526ada63e
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-05-15 15:51:05 +0800

    refactor(groups): add groups API

--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -3,14 +3,17 @@ import { GroupsService } from './groups.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Group } from './entities/group.entity';
 import { GroupUser } from './entities/group-user.entity';
+import { NamespacesModule } from 'src/namespaces/namespaces.module';
+import { GroupsController } from './groups.controller';
 
 @Module({
   providers: [GroupsService],
   exports: [GroupsService],
-  controllers: [],
+  controllers: [GroupsController],
   imports: [
     TypeOrmModule.forFeature([Group]),
     TypeOrmModule.forFeature([GroupUser]),
+    NamespacesModule,
   ],
 })
 export class GroupsModule {}

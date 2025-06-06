54f350598363d880b33eb4a72e28afc62274e9eb
Lucien <lucien@lucien.ink>
2025-06-07 01:11:20 +0800

    Remove '_' from charset

--- src/utils/generate_id.ts
+++ src/utils/generate_id.ts
@@ -2,7 +2,7 @@ import { customAlphabet } from 'nanoid';
 
 export default function generateId(size = 16) {
   const urlAlphabet =
-    'useandom26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict';
+    'useandom26T198340PX75pxJACKVERYMINDBUSHWOLFGQZbfghjklqvwyzrict';
   const nanoid = customAlphabet(urlAlphabet, size);
   return nanoid();
 }

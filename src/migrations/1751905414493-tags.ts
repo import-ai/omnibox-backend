21799eba3c475c56bb9004197f0cfcacbd91fdec
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-07-14 17:25:28 +0800

    refactor(migrations): update tags table

--- src/migrations/1751905414493-tags.ts
+++ src/migrations/1751905414493-tags.ts
@@ -1,16 +1,37 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { BaseColumns } from './base-columns';
 
 export class Tags1751905414493 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE TABLE tags
-(
-    "id"           CHARACTER VARYING(6) PRIMARY KEY,
-    "name"         CHARACTER VARYING(20)    NOT NULL,
-    "namespace_id" CHARACTER VARYING(6)     NOT NULL REFERENCES namespaces (id),
-    "created_at"   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    "updated_at"   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    "deleted_at"   TIMESTAMP WITH TIME ZONE
-)`);
+    const table = new Table({
+      name: 'tags',
+      columns: [
+        {
+          name: 'id',
+          type: 'character varying',
+          isPrimary: true,
+        },
+        {
+          name: 'namespace_id',
+          type: 'character varying',
+          isNullable: false,
+        },
+        {
+          name: 'name',
+          type: 'character varying',
+          isNullable: false,
+        },
+        ...BaseColumns(),
+      ],
+      foreignKeys: [
+        {
+          columnNames: ['namespace_id'],
+          referencedTableName: 'namespaces',
+          referencedColumnNames: ['id'],
+        },
+      ],
+    });
+    await queryRunner.createTable(table, true, true, true);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {

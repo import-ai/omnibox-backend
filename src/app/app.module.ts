7a1d6e7f3f7da47e2d5806a99cd957362e0ce1cc
hewenguang <wenguang.fe@gmail.com>
2025-07-30 20:16:24 +0800

    feat(bot): return static page for search engine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,8 @@ import { ConversationsModule } from 'omniboxd/conversations/conversations.module
 import { MessagesModule } from 'omniboxd/messages/messages.module';
 import { SearchModule } from 'omniboxd/search/search.module';
 import { InvitationsModule } from 'omniboxd/invitations/invitations.module';
-import { LoggerMiddleware } from './logger.middleware';
+import { LoggerMiddleware } from 'omniboxd/middleware/logger.middleware';
+import { SeoMiddleware } from 'omniboxd/middleware/seo.middleware';
 import { UserOptions1751904560034 } from 'omniboxd/migrations/1751904560034-user-options';
 import { UserBindings1752652489640 } from 'omniboxd/migrations/1752652489640-user-bindings.ts';
 import { Tags1751905414493 } from 'omniboxd/migrations/1751905414493-tags';
@@ -37,7 +38,7 @@ import { AttachmentsModule } from 'omniboxd/attachments/attachments.module';
 
 @Module({})
 export class AppModule implements NestModule {
-  static forRoot(extraMigrations: Function[]): DynamicModule {
+  static forRoot(extraMigrations: Array<() => void>): DynamicModule {
     return {
       module: AppModule,
       controllers: [AppController],
@@ -113,6 +114,6 @@ export class AppModule implements NestModule {
   }
 
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(LoggerMiddleware, SeoMiddleware).forRoutes('*');
   }
 }

8f79943cd9c8cad866620ef627bb4f541292fa15
Lucien <lucien@lucien.ink>
2025-08-01 07:46:48 +0800

    feat(ci): Support coverage report, e2e includes all tests

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^omniboxd/(.*)$': '<rootDir>/src/$1',
+    '^test/(.*)$': '<rootDir>/test/$1',
+  },
+};

bf7c7011ce490400fe4aebe00b847a239e200b45
Lucien <lucien@lucien.ink>
2025-07-31 08:16:30 +0800

    fix(ci): Fix runtime image tag

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,8 @@
-function randomChoice(choices) {
+function randomChoice(choices: string) {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function customAlphabet(alphabet, size) {
+function customAlphabet(alphabet: string, size: number) {
   return () => {
     let result = '';
     for (let i = 0; i < size; i++) {

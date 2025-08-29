f5436e7b791c4cbf1372db80ffdcc844be739b4a
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-08-29 13:55:06 +0800

    refactor(trace): update event props

--- src/trace/dto/trace-message.dto.ts
+++ src/trace/dto/trace-message.dto.ts
@@ -8,22 +8,22 @@ export class TraceMessageDto {
   @Expose({ name: 'event_name' })
   eventName: string;
 
+  @Expose({ name: 'event_props' })
+  eventProps?: string;
+
   @Expose({ name: 'user_id' })
   userId?: string;
 
   @Expose({ name: 'user_agent' })
   userAgent?: string;
 
-  @Expose()
-  props: string;
-
   static fromEvent(event: TraceEventDto, userId?: string, userAgent?: string) {
     const message = new TraceMessageDto();
     message.timestamp = Date.now();
     message.eventName = event.name;
+    message.eventProps = JSON.stringify(event.props);
     message.userId = userId;
     message.userAgent = userAgent;
-    message.props = JSON.stringify(event.props);
     return message;
   }
 }

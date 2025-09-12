2e9ba25a5ea4df40a640101b06945c435a988f8f
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-09-12 18:54:18 +0800

    refactor(resources): add getAllSubResources

--- src/shared-resources/dto/shared-resource-meta.dto.ts
+++ src/shared-resources/dto/shared-resource-meta.dto.ts
@@ -1,18 +1,33 @@
+import { Expose } from 'class-transformer';
+import { ResourceMetaDto } from 'omniboxd/resources/dto/resource-meta.dto';
 import {
   Resource,
   ResourceType,
 } from 'omniboxd/resources/entities/resource.entity';
 
 export class SharedResourceMetaDto {
+  @Expose()
   id: string;
+
+  @Expose()
   name: string;
-  resource_type: ResourceType;
+
+  @Expose({ name: 'resource_type' })
+  resourceType: ResourceType;
 
   static fromEntity(resource: Resource): SharedResourceMetaDto {
     const dto = new SharedResourceMetaDto();
     dto.id = resource.id;
     dto.name = resource.name;
-    dto.resource_type = resource.resourceType;
+    dto.resourceType = resource.resourceType;
+    return dto;
+  }
+
+  static fromResourceMeta(resource: ResourceMetaDto): SharedResourceMetaDto {
+    const dto = new SharedResourceMetaDto();
+    dto.id = resource.id;
+    dto.name = resource.name;
+    dto.resourceType = resource.resourceType;
     return dto;
   }
 }

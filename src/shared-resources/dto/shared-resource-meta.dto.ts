158c279c5c6ccf8db4bc6bfaef7fcccaf317d5ce
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-09-15 16:39:50 +0800

    refactor(resources): update shared-resource-meta

--- src/shared-resources/dto/shared-resource-meta.dto.ts
+++ src/shared-resources/dto/shared-resource-meta.dto.ts
@@ -1,4 +1,4 @@
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 import { ResourceMetaDto } from 'omniboxd/resources/dto/resource-meta.dto';
 import {
   Resource,
@@ -15,11 +15,21 @@ export class SharedResourceMetaDto {
   @Expose({ name: 'resource_type' })
   resourceType: ResourceType;
 
+  @Expose({ name: 'created_at' })
+  @Transform(({ value }) => value.toISOString())
+  createdAt: Date;
+
+  @Expose({ name: 'updated_at' })
+  @Transform(({ value }) => value.toISOString())
+  updatedAt: Date;
+
   static fromEntity(resource: Resource): SharedResourceMetaDto {
     const dto = new SharedResourceMetaDto();
     dto.id = resource.id;
     dto.name = resource.name;
     dto.resourceType = resource.resourceType;
+    dto.createdAt = resource.createdAt;
+    dto.updatedAt = resource.updatedAt;
     return dto;
   }
 
@@ -28,6 +38,8 @@ export class SharedResourceMetaDto {
     dto.id = resource.id;
     dto.name = resource.name;
     dto.resourceType = resource.resourceType;
+    dto.createdAt = resource.createdAt;
+    dto.updatedAt = resource.updatedAt;
     return dto;
   }
 }

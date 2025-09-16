a1133880c5d46f06200d5390cb570cee7cb4b65b
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-09-16 17:01:37 +0800

    refactor(shares): update public-share-info

--- src/shares/shares.module.ts
+++ src/shares/shares.module.ts
@@ -6,10 +6,10 @@ import {
   PublicSharesController,
   ResourceSharesController,
 } from './shares.controller';
-import { NamespaceResourcesModule } from 'omniboxd/namespace-resources/namespace-resources.module';
+import { ResourcesModule } from 'omniboxd/resources/resources.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Share]), NamespaceResourcesModule],
+  imports: [TypeOrmModule.forFeature([Share]), ResourcesModule],
   providers: [SharesService],
   exports: [SharesService],
   controllers: [ResourceSharesController, PublicSharesController],

8780aa8d48267ea8a5450f15a70bb5a3a10bbc78
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-10-10 20:33:50 +0800

    fix(websocket): fix module

--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -3,9 +3,10 @@ import { WizardGateway } from 'omniboxd/websocket/wizard.gateway';
 import { WsJwtGuard } from 'omniboxd/websocket/ws-jwt.guard';
 import { WizardModule } from 'omniboxd/wizard/wizard.module';
 import { AuthModule } from 'omniboxd/auth';
+import { SharesModule } from 'omniboxd/shares/shares.module';
 
 @Module({
-  imports: [WizardModule, AuthModule],
+  imports: [WizardModule, AuthModule, SharesModule],
   providers: [WizardGateway, WsJwtGuard],
   exports: [WizardGateway, WsJwtGuard],
 })

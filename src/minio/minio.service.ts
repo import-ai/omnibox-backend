baf6ebe45a81ff935394abc7f55c60b1722304b8
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-11-10 20:56:41 +0800

    refactor(resources): reformat code

--- src/minio/minio.service.ts
+++ src/minio/minio.service.ts
@@ -65,7 +65,8 @@ export class MinioService implements OnModuleInit {
     }
 
     const s3Region = configService.get<string>('OBB_S3_REGION', 'us-east-1');
-    const forcePathStyle = configService.get<string>('OBB_S3_FORCE_PATH_STYLE', 'false') === 'true';
+    const forcePathStyle =
+      configService.get<string>('OBB_S3_FORCE_PATH_STYLE', 'false') === 'true';
     this.s3Client = new S3Client({
       region: s3Region,
       credentials: {
@@ -228,8 +229,13 @@ export class MinioService implements OnModuleInit {
     try {
       await this.s3Client.send(new HeadBucketCommand({ Bucket: this.bucket }));
     } catch (error: any) {
-      if (error.name === 'NotFound' || error.$metadata?.httpStatusCode === 404) {
-        await this.s3Client.send(new CreateBucketCommand({ Bucket: this.bucket }));
+      if (
+        error.name === 'NotFound' ||
+        error.$metadata?.httpStatusCode === 404
+      ) {
+        await this.s3Client.send(
+          new CreateBucketCommand({ Bucket: this.bucket }),
+        );
       } else {
         throw error;
       }

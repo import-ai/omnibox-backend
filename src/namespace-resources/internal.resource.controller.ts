e194d2e7e44fbc15d8ddab883d9c31b24d590984
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-11-14 15:40:24 +0800

    refactor(files): remove legacy download api

--- src/namespace-resources/internal.resource.controller.ts
+++ src/namespace-resources/internal.resource.controller.ts
@@ -1,5 +1,4 @@
-import { Response } from 'express';
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { NamespaceResourcesService } from 'omniboxd/namespace-resources/namespace-resources.service';
 import { Public } from 'omniboxd/auth/decorators/public.auth.decorator';
 
@@ -8,12 +7,6 @@ export class InternalResourcesController {
   constructor(
     private readonly namespaceResourcesService: NamespaceResourcesService,
   ) {}
-
-  @Public()
-  @Get('resources/files/:id')
-  async downloadFile(@Param('id') resourceId: string, @Res() res: Response) {
-    return await this.namespaceResourcesService.fileResponse(resourceId, res);
-  }
 
   @Public()
   @Get('namespaces/:namespaceId/resources/:resourceId/file')

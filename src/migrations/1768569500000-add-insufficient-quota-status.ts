d7c288276c7b5d4c40ef99be9eaa587afdbe14f3
Yichen Zhao <yichenzhao.yz@gmail.com>
2026-01-21 19:15:00 +0800

    refactor(migrations): simplify class definition for AddInsufficientQuotaStatus migration

--- src/migrations/1768569500000-add-insufficient-quota-status.ts
+++ src/migrations/1768569500000-add-insufficient-quota-status.ts
@@ -1,8 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddInsufficientQuotaStatus1768569500000
-  implements MigrationInterface
-{
+export class AddInsufficientQuotaStatus1768569500000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       ALTER TYPE task_status ADD VALUE IF NOT EXISTS 'insufficient_quota';

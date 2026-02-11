b25e72316f39dd6430b42c547c45bf5e502c075b
Yichen Zhao <yichenzhao.yz@gmail.com>
2026-02-11 16:45:27 +0800

    refactor(resources): fix usages (#311)
    * refactor(resources): remove getSubResources
    
    * refactor(namespace-resources): remove SidebarChildDto
    
    * feat(resources): enhance resource query to include additional fields
    
    * refactor(resources): fix restore
    
    * refactor(resources): fix dto
    
    * refactor(resources): fix parent id

--- src/namespace-resources/dto/trash-item.dto.ts
+++ src/namespace-resources/dto/trash-item.dto.ts
@@ -1,16 +1,16 @@
 import { Expose, Transform } from 'class-transformer';
 import { Resource } from 'omniboxd/resources/entities/resource.entity';
 
-import { SidebarChildDto } from './sidebar-child.dto';
+import { ResourceSummaryDto } from './resource-summary.dto';
 
-export class TrashItemDto extends SidebarChildDto {
+export class TrashItemDto extends ResourceSummaryDto {
   @Expose({ name: 'deleted_at' })
   @Transform(({ value }) => value?.toISOString())
   deletedAt: Date;
 
   static fromEntity(resource: Resource, hasChildren: boolean): TrashItemDto {
     const dto = new TrashItemDto();
-    Object.assign(dto, SidebarChildDto.fromEntity(resource, hasChildren));
+    Object.assign(dto, ResourceSummaryDto.fromEntity(resource, hasChildren));
     dto.deletedAt = resource.deletedAt!;
     return dto;
   }

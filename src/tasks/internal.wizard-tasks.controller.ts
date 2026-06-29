c4ee8a590411c08803ed4a8b9c585eb3e414a0a3
Yichen Zhao <yichenzhao.yz@gmail.com>
2026-06-29 10:59:22 +0800

    feat(tasks): add resource tasks controller and dto

--- src/tasks/internal.wizard-tasks.controller.ts
+++ src/tasks/internal.wizard-tasks.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { Public } from 'omniboxd/auth/decorators/public.auth.decorator';
-import { InternalTaskDto } from 'omniboxd/tasks/dto/task.dto';
+import {
+  InternalResourceTaskDto,
+  InternalTaskDto,
+} from 'omniboxd/tasks/dto/task.dto';
 import { TasksService } from 'omniboxd/tasks/tasks.service';
 
 @Controller('internal/api/v1/wizard/tasks')
@@ -11,5 +14,25 @@ export class InternalWizardTasksController {
   @Get(':id')
   async getTaskById(@Param('id') id: string) {
     return InternalTaskDto.fromEntity(await this.tasksService.get(id));
+  }
+}
+
+@Controller(
+  'internal/api/v1/namespaces/:namespaceId/resources/:resourceId/tasks',
+)
+export class InternalResourceTasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Public()
+  @Get()
+  async getResourceTasks(
+    @Param('namespaceId') namespaceId: string,
+    @Param('resourceId') resourceId: string,
+  ): Promise<InternalResourceTaskDto[]> {
+    const tasks = await this.tasksService.getTasksByResourceId(
+      namespaceId,
+      resourceId,
+    );
+    return tasks.map((task) => InternalResourceTaskDto.fromTaskMeta(task));
   }
 }
